d88c0d867b4a1db2c163332388bb2e24e972f270
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-17 19:49:32 +0300

    added the try/except Http404 error in delete_store_item view

--- stores/views.py
+++ stores/views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from stores import models
 from stores.forms import StoreItemForm
+from django.http import Http404
 
 
 def get_store_items(request: HttpRequest) -> HttpResponse:
@@ -39,7 +40,10 @@ def update_store_item(request, item_id):
     return render(request, "update-store-item.html", context)
 
 def delete_store_item(request, item_id):
-    store_item = models.StoreItem.objects.get(id=item_id)
+    try:
+        store_item = models.StoreItem.objects.get(id=item_id)
+    except models.StoreItem.DoesNotExist:
+        raise Http404()
     store_item.delete()
     return redirect("store_item_list")
 
