f0cfc96d965e27b02f992d473ba396917cb40298
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-17 16:23:25 +0300

    finished 'update view'

--- bakery/urls.py
+++ bakery/urls.py
@@ -21,5 +21,6 @@ from stores import views as store_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("items/", store_views.get_store_items, name="store-item-list"),
-    path("create/", store_views.create_store_item, name="create-store-item")
+    path("create/", store_views.create_store_item, name="create-store-item"),
+    path("update/<int:item_id>/", store_views.update_store_item, name="update-store-item"),
 ]
