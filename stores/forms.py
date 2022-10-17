86c9810b14cea4dcc434370f99a97d7ef4f9c3f1
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-17 13:41:11 +0300

    finished 'create form' section

---
+++ stores/forms.py
@@ -0,0 +1,10 @@
+# from socket import fromshare
+from django import forms
+from .models import StoreItem
+
+class StoreItemForm(forms.ModelForm):
+    class Meta:
+        model = StoreItem
+        fields = ["name", "description", "kind", "price"]
+
+
